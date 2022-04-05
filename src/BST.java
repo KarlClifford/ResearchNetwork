a8567ab5f6ac749742902baaa9989dcba61a3fb2
KarlClifford <65084552+KarlClifford@users.noreply.github.com>
2022-04-05 20:41:50 +0100

    Added comments and some new methods.

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,6 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  * BTS.
  * @version 1.0
@@ -21,9 +24,11 @@ public class BST {
 
     }
 
+    /**
+     * Inserts a researcher into the BST.
+     * @param p is the profile to be added to the BST.
+     */
     public void insertResearcher(Profile p) {
-        //System.out.println("INSERTING + " + p.getFamilyNames() + " " + p.getGivenNames());
-        //System.out.println(p);
 
         // Make new node to add to tree from p parameter
         final BSTNode NEW_NODE = new BSTNode(p);
@@ -37,29 +42,16 @@ public class BST {
         }
     }
 
+    /**
+     * Recursive method to set a child element in the BST depending on whether the child comes alphabetically
+     * before or after the parent.
+     * @param parent the parent node.
+     * @param child the child node to be added as a child of the parent on either the Left or Right side.
+     */
     private void setNext(BSTNode parent, BSTNode child) {
-
-        // The integer representation of the first letter of the parent researcher's name.
-        //int currentResearcher = parent
-                //.getResearcher()
-                //.getFamilyNames()
-                //.toLowerCase()
-                //.toCharArray()[0];
-
-        // The integer representation of the first letter of the child researcher's name.
-        //int newResearcher = child
-                //.getResearcher()
-                //.getFamilyNames()
-                //.toLowerCase()
-                //.toCharArray()[0];
-
-        //System.out.println("RUNNING SET NEXT!");
-        //System.out.println("PARENT: " + parent.getResearcher().getFamilyNames());
-        //System.out.println("CHILD: "+ child.getResearcher().getFamilyNames());
 
         // The integer comparison of the parent node to the child node.
         int comparison = child.getResearcher().getFamilyNames().compareTo(parent.getResearcher().getFamilyNames());
-        //System.out.println(comparison);
 
         // Check if the new researcher's name should be ordered left or right to the current node.
         if (comparison <= 0) {
@@ -83,5 +75,47 @@ public class BST {
         }
 
 
+    }
+
+    /**
+     * Prints the entire BST in alphabetical order of family names.
+     * @return a String of alphabetically ordered profiles.
+     */
+    public String printAlphabetical() {
+
+        // Set to pass into the getNext() function that will store researchers last name followed by their first name.
+        SortedSet<String> profiles = new TreeSet<>();
+
+        // This is the data we will return from this method after profiles have been formatted correctly.
+        StringBuilder toOutput = new StringBuilder();
+
+        // Iterate over each profile and make sure that each element will be printed on a new line.
+        for (String profile : getNext(root, profiles)) {
+            toOutput.append(profile).append("\n");
+        }
+
+        return toOutput.toString();
+    }
+
+    /**
+     * Recursive method that scans the entire tree and returns each researcher it finds in a sorted set.
+     * @param node is the node to scan.
+     * @param profiles is the set of profiles to add results to.
+     * @return a sorted set of researchers names in the format <FamilyName>, <GivenName>
+     */
+    private SortedSet<String> getNext(BSTNode node, SortedSet<String> profiles) {
+
+        profiles.add(node.getResearcher().getFamilyNames() + ", " + node.getResearcher().getGivenNames());
+
+        // Check the left child of this node, if it's not null get the child node.
+        if (node.getL() != null) {
+            getNext(node.getL(), profiles);
+        }
+        // Check the right child of this node, if it's not null get the child node.
+        if (node.getR() != null) {
+            getNext(node.getR(), profiles);
+        }
+
+        return profiles;
     }
 }
