4cd945c692368b3250f638fb22d60418e518d814
KarlClifford <65084552+KarlClifford@users.noreply.github.com>
2022-04-06 00:39:44 +0100

    Working version of code

--- src/Graph.java
+++ src/Graph.java
@@ -28,26 +28,34 @@ public class Graph {
         readCollaborators(filename);
     }
 
+    /**
+     * reads collaborators from a given file.
+     * @param filename the file to read.
+     */
     private void readCollaborators(String filename) {
         // Read the file and add each collaborator pair.
         try {
             File file = new File(filename);
-
             Scanner in = new Scanner(file);
 
-            //
+            // Read each line and make a new collaborator pair from this.
             while (in.hasNextLine()) {
                 String[] collaborators = in.nextLine().split(",");
                 addCollaborator(collaborators);
             }
         } catch (FileNotFoundException e) {
+            // File name is incorrect or file is missing, exit the program gracefully.
             System.out.println("File not found");
             System.exit(1);
         }
     }
 
+    /**
+     * Adds collaborators to profiles in the BST.
+     * @param collaborators collaborators to add to the given profiles.
+     */
     private void addCollaborator(String[] collaborators) {
-        // Find profiles.
+        // Find profiles from the collaborators that were passed into this method.
         Profile p1 = tree.getProfile(collaborators[0]);
         Profile p2 = tree.getProfile(collaborators[1]);
 
@@ -56,63 +64,58 @@ public class Graph {
             // Add these profiles as collaborators
             p1.collaborate(p2);
         }
-
-        //TODO: Place collaborators back into the BST?
-        //this.tree.updateTree(p1);
-        //this.tree.updateTree(p2);
-
-
     }
 
+    /**
+     * Finds an influencer profile.
+     * This is any profile that meets the following criteria:
+     * 1. Has collaborated with the most people.
+     * 2. Is not already collaborating with the given profile.
+     * @param familyNames profile that we will find an influencer for.
+     * @return an influencer.
+     */
     private Profile getInfluencer(String familyNames) {
-        //Get the profile in question.
+        // Get the profile from input. We will compare this to the profiles in the BST.
         Profile p1 = tree.getProfile(familyNames);
-        Profile p2 = null;
+        Profile p2;
 
-        //Search through the BST for a suitable influencer.
-        //ArrayList<Profile> profiles = new ArrayList<>();
-        //tree.searchTree(tree.getRoot(), profiles);
-
-        /*int profileIndex = 0;
-        Profile profileOne = null;
-        Profile profileTwo = null;
-
-        while(profileIndex < profiles.size()) {
-            profileOne = profiles.get(profileIndex);
-            if(profileIndex + 1 > profiles.size()) {
-                profileTwo = profiles.get(profileIndex + 1);
-            } else {
-                // Increment profileIndex to exit loop
-                profileIndex ++;
-            }
-
-            if (profileTwo != null && (profileOne.equals(thisProfile)
-                    || profileOne.hasCollaboratedWith(thisProfile)
-                    || (profileOne.numOfCollabs() < profileTwo.numOfCollabs()))) {
-                // Get the next profile
-                profileIndex++;
-            }
-        }
-
-        return profileOne;*/
-
+        // Pass an ArrayList into the search tree method to populate it with all the profiles in the BST.
         ArrayList<Profile> profiles = new ArrayList<>();
         tree.searchTree(tree.getRoot(), profiles);
-        Comparator<Profile> comparator = new ProfileComparator();
-        PriorityQueue<Profile> prio = new PriorityQueue<>(comparator);
-        prio.addAll(profiles);
 
-        while (!prio.isEmpty()) {
-            p2 = prio.poll();
+        /*
+         * Make a comparator that will allow our priority queue to order
+         * from the greatest number of collaborators to least.
+         */
+        Comparator<Profile> comparator = new ProfileComparator();
+        PriorityQueue<Profile> queue = new PriorityQueue<>(comparator);
+
+        queue.addAll(profiles);
+
+        /*
+         * Look at every top element in the queue, it is likely that we will have a successful hit within the first
+         * few elements as the queue is already ordered.
+         */
+        while (!queue.isEmpty()) {
+
+            // Remove the top element of the queue and store it for comparison.
+            p2 = queue.poll();
+
+            // Check that p2 isn't a duplicate of p1 and that these profiles are not already collaborating.
             if (!p1.equals(p2) && !p1.hasCollaboratedWith(p2)) {
                 return p2;
             }
         }
 
-        //No suituble profile found!
+        // No profile in our BST meets the criteria to be an influencer so return our initial p1 profile.
         return p1;
     }
 
+    /**
+     * Use to find an influencer from outside this class.
+     * @param familyName profile that we will find an influencer for.
+     * @return an influencer.
+     */
     public Profile findInfluencer(String familyName) {
         return getInfluencer(familyName);
     }
